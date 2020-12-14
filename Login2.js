18b6a8b5d2e310f126a964beb10befeba354eeb1
OneZeroZeroOneOne <50696667+OneZeroZeroOneOne@users.noreply.github.com>
2020-12-14 20:20:51 +0300

    Update Login2.js
    logn

--- Login2.js
+++ Login2.js
@@ -1,6 +1,36 @@
-(function(){function getJwt(loginData,success,error,complete){$.ajax({url:loginData.path,type:'GET',data:{email:loginData.user,password:loginData.pass},success:function(data){success&&success(data.access_token);},error:function(jqXhr,err,msg){error&&error(JSON.parse(jqXhr.responseText).error_description);},complete:complete});}
-function setJwt(key){const constantMock=window.fetch;window.fetch=function(){console.log(arguments);console.log(arguments[1].headers);arguments[1].headers["Authorization"]=key;console.log(arguments[1].headers);return constantMock.apply(this,arguments);}}
-$(function(){(function(){var styles=`
+(function() {
+    function getJwt(loginData, success, error, complete) {
+        $.ajax({
+            url: loginData.path,
+            type: 'GET',
+            data: {
+                login: loginData.user,
+                password: loginData.pass
+            },
+            success: function(data) {
+                console.log(data.token);
+                success && success(data.token);
+            },
+            error: function(jqXhr, err, msg) {
+                error && error(JSON.parse(jqXhr.responseText).error_description);
+            },
+            complete: complete
+        });
+    }
+
+    function setJwt(key) {
+        const constantMock = window.fetch;
+        window.fetch = function() {
+            console.log(arguments);
+            console.log(arguments[1].headers);
+            arguments[1].headers["Authorization"] = key;
+            console.log(arguments[1].headers);
+            return constantMock.apply(this, arguments);
+        }
+    }
+    $(function() {
+        (function() {
+            var styles = `
                 <style>
                 #sa-setting{
                     display: inline-block !important;
@@ -29,7 +59,9 @@ $(function(){(function(){var styles=`
                     border: 2px inset;
                 }
             </style>
-            `;$('head').append(styles);var settingTemplate=`
+            `;
+            $('head').append(styles);
+            var settingTemplate = `
                 <div id="sa-setting">
                     <input id="sa-path" placeholder="Path" value="/Token">
                     <br>
@@ -40,5 +72,51 @@ $(function(){(function(){var styles=`
                     <span id="sa-btn-login" class="sa-btn">Login</span>
                     <span id="sa-btn-logout" class="sa-btn" style="display: none">Logout</span>
                 </div>
-            `;$('body').append(settingTemplate);$('<div id="sa-btn-setting" class="sa-btn">Login</div>').click(function(){$('#sa-setting').fadeToggle();}).appendTo('#api_selector');})();function login(loginData){$('#sa-btn-setting').text('Working...');getJwt(loginData,function(jwt){$('#sa-btn-setting').text('Hi '+loginData.user).css('background','#0f6ab4');$('#sa-setting').fadeOut();$('#sa-btn-logout').fadeIn();setJwt(jwt);},function(err){$('#sa-btn-setting').text('Failed').css('background','#a41e22');setJwt('');alert(err);},function(){});}
-$('#sa-btn-logout').click(function(){setJwt('');window.localStorage.removeItem('sa-login-data');$('#sa-username').val('');$('#sa-password').val('');$(this).fadeOut();});$('#sa-btn-login').click(function(){var loginData={path:$('#sa-path').val(),user:$('#sa-username').val(),pass:$('#sa-password').val()};window.localStorage.setItem('sa-login-data',JSON.stringify(loginData));login(loginData);});(function(){var oldLoginData=window.localStorage.getItem('sa-login-data');if(oldLoginData){oldLoginData=JSON.parse(oldLoginData);$('#sa-path').val(oldLoginData.path);$('#sa-username').val(oldLoginData.user);$('#sa-password').val(oldLoginData.pass);login(oldLoginData);}})();});})();
+            `;
+            $('body').append(settingTemplate);
+            $('<div id="sa-btn-setting" class="sa-btn">Login</div>').click(function() {
+                $('#sa-setting').fadeToggle();
+            }).appendTo('#api_selector');
+        })();
+
+        function login(loginData) {
+            $('#sa-btn-setting').text('Working...');
+            getJwt(loginData, function(jwt) {
+                $('#sa-btn-setting').text('Logined').css('background', '#0f6ab4');
+                $('#sa-setting').fadeOut();
+                $('#sa-btn-logout').fadeIn();
+                setJwt(jwt);
+            }, function(err) {
+                $('#sa-btn-setting').text('Failed').css('background', '#a41e22');
+                setJwt('');
+                alert(err);
+            }, function() {});
+        }
+        $('#sa-btn-logout').click(function() {
+            setJwt('');
+            window.localStorage.removeItem('sa-login-data');
+            $('#sa-username').val('');
+            $('#sa-password').val('');
+            $(this).fadeOut();
+        });
+        $('#sa-btn-login').click(function() {
+            var loginData = {
+                path: $('#sa-path').val(),
+                user: $('#sa-username').val(),
+                pass: $('#sa-password').val()
+            };
+            window.localStorage.setItem('sa-login-data', JSON.stringify(loginData));
+            login(loginData);
+        });
+        (function() {
+            var oldLoginData = window.localStorage.getItem('sa-login-data');
+            if (oldLoginData) {
+                oldLoginData = JSON.parse(oldLoginData);
+                $('#sa-path').val(oldLoginData.path);
+                $('#sa-username').val(oldLoginData.user);
+                $('#sa-password').val(oldLoginData.pass);
+                login(oldLoginData);
+            }
+        })();
+    });
+})();
